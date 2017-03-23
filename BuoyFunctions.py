9f9359f88c21d5ef3a8c2c5069fd6161a3cab004
Joe Rippke <rippk003@umn.edu>
2017-03-23 16:36:21 -0500

    added comments to the code

--- BuoyFunctions.py
+++ BuoyFunctions.py
@@ -4,23 +4,26 @@
 Created on Thu Mar 23 10:05:56 2017
 
 @author: JoeRippke
+This file contains functions for use with BuoySummer.py
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import csv
-from scipy import stats
 
+# This funciton opens .txt files and returns a pandas dataframe of dates and
+#temperatures
 def OpenFile(region,year):
     
     file = open((region+str(year)+'.txt'),'r')
-    
+    # the formatting of the .txt files has changed twice over the years
+    # this if statement handles each of the formats
     if year <= 1998:
-        columns = ['YY','MM','DD','WTMP']
-        dates = ['YY','MM','DD']
+        columns = ['YY','MM','DD','WTMP'] # defines which columns to use
+        dates = ['YY','MM','DD'] # defines the date information columns
         data = pd.read_table(file,sep = '\s+',header = 0,
                usecols = columns, parse_dates = {'date': dates},
-               index_col = ['date'], na_values = '999.0')
+               index_col = ['date'], na_values = '999.0') # creates the 
+                             # pandas dataframe
         
     elif year >= 1999 and year <= 2006:
         columns = ['YYYY','MM','DD','WTMP']
@@ -36,16 +39,21 @@ def OpenFile(region,year):
                usecols = columns, parse_dates = {'date': dates},
                index_col = ['date'], na_values = '999.0')
         
-    pd.to_numeric(data['WTMP'],errors='coerce')       
+    pd.to_numeric(data['WTMP'],errors='coerce') # converts the temperature
+# values to floating point objects       
     return(data)
 
+# this function identifies the summer period and returns the mean temperature
+# over that period
 def SummerMean(data,year):
-    start = str(year)+'0701'
+    start = str(year)+'0701' # format the start and end dates for use with pandas
     end = str(year)+'0930'
-    summer = data.loc[start:end]
-    mean = summer.mean()
+    summer = data.loc[start:end] # pandas command to isolate the summer
+    mean = summer.mean() # calculate the mean
     return(mean)
 
+# this function creates a scatter plot of the averaged temperatures over the 
+# years for which data is available
 def SummerPlot(year,data):
     plt.close('all')
     plt.scatter(year,data)
@@ -53,7 +61,8 @@ def SummerPlot(year,data):
     plt.xlabel('Year')
     plt.ylabel('Temperature [°C]')
     plt.title('Lake Superior Summertime (Jul-Sep)\nAverage Surface Water Temperature')
-    
+
+# This function saves a .csv file of the averaged temperatures
 def SaveFile(year,data):
     with open('SummerMeans.csv', 'w') as f:
         writer = csv.writer(f, delimiter=',')
--- BuoySummer.py
+++ BuoySummer.py
@@ -4,23 +4,43 @@
 Created on Wed Mar 22 15:15:48 2017
 
 @author: JoeRippke
+
+This program parses data contained in .txt files produced by NOAA buoys
+deployed on Lake Superior. All of the .txt files in the git repository are
+necessary to run this program.
+
+The desired outcome is that this program should output a .csv file that
+contains one value for each year of available buoy data. This value represents
+the temperature of the surface waters averaged over the three buoys and
+averaged over the summer defined as July, August and September.
+
+The buoy data is available at: http://www.ndbc.noaa.gov/
+buoys: 45006, 45001 and 45004
+
+This is written in python 3
 """
 import BuoyFunctions as bf
 import numpy as np
 
-region = ['w','c','e']
+region = ['w','c','e'] # each region corresponds to a buoy
 year = [1981+x for x in range(35)]
-SummerMeans = []
+SummerMeans = [] #empty list where averaged temps are temporarily stored
 
-for yr in range(len(year)):
-    means =[]
-    for reg in range(len(region)):
-        data = bf.OpenFile(region[reg],year[yr])
-        mean = bf.SummerMean(data,year[yr])
-        means.append(mean)
-    print(year[yr])
-    SummerMeans.append(float(np.mean(means)))
+for yr in range(len(year)): # loop over all of the years in the range
+    means =[] # temporary list that hold the mean temperatures of the
+    #individual buoys
+    for reg in range(len(region)): # loop over each of the buoys
+        data = bf.OpenFile(region[reg],year[yr]) # open a datafile and isolate
+        # the water temperature column, also define NaN values as 999.0
+        mean = bf.SummerMean(data,year[yr]) # calculate the mean temperature
+        #for the summer period
+        means.append(mean) # hold on to the mean value for now
+    print(year[yr]) # print the year, just so I can tell it's doing something
+    # as it runs
+    SummerMeans.append(float(np.mean(means))) # average the values from the
+    # three buoys for a single year and append them to a list
 
-bf.SaveFile(year,SummerMeans)
-bf.SummerPlot(year,SummerMeans)
+bf.SaveFile(year,SummerMeans) # save a .csv file of the temperature data
+# indexed by year
+bf.SummerPlot(year,SummerMeans) # *bonus function plots the temperature data
 
